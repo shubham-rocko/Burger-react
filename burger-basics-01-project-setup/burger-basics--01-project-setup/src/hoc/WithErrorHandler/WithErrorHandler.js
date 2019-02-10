2c89f4d1d10f0ea1a326a4089422122ee6b8af89
shubham singh <shubhamit3038@gmail.com>
2019-02-10 15:15:39 +0530

    add axios and http req and res using firebase

---
+++ burger-basics-01-project-setup/burger-basics--01-project-setup/src/hoc/WithErrorHandler/WithErrorHandler.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+import Aux from '../Aux/Aux';
+import Modal from '../../components/UI/Modal/Modal';
+
+const withErrorHandler = (WrappedComponent, axios) => {
+
+    return class extends Component {
+
+        state = {
+            error: null
+        };
+
+        componentDidMount() {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
+                this.setState({ error: null });
+                return req;
+            });
+
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
+                this.setState({ error: error });
+            });
+        }
+
+        componentDidUnmount() {
+            axios.interceptors.request.eject(this.reqInterceptor); //
+            axios.interceptors.response.eject(this.resInterceptor); //remove interceptor when component did Unmout just because of memory leakage
+        }
+
+        confirmedErroeHandler = () => {
+            this.setState({ error: null })
+        }
+
+        render() {
+            return (
+                <Aux>
+                    <Modal show={this.state.error}
+                        modalClosed={this.confirmedErroeHandler}>
+                        {this.state.error ? this.state.error.message : null}
+                    </Modal>
+                    <WrappedComponent {...this.props} />
+                </Aux>
+            )
+        }
+    }
+}
+
+export default withErrorHandler;

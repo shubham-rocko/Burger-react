d5a35a2388189f221a998ef696de05717ebb6542
shubham singh <shubhamit3038@gmail.com>
2019-02-25 13:09:58 +0530

    fetching orders

--- burger-basics-01-project-setup/burger-basics--01-project-setup/src/containers/Orders/Orders.js
+++ burger-basics-01-project-setup/burger-basics--01-project-setup/src/containers/Orders/Orders.js
@@ -1,8 +1,33 @@
 import React, {Component} from 'react';
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner';
+import WithErrorHandler from '../../hoc/WithErrorHandler/WithErrorHandler';
 
 class Orders extends Component {
+
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount(){
+        axios.get('orders.json')
+        .then(res => {
+            const fetchedOrders = [];
+            for (let key of res.data){
+                fetchedOrders.push({
+                    ...res.data[key],
+                    id: key
+                });
+            }
+            this.setState({loading: false, orders: fetchedOrders});
+        }).catch((err) => {
+            this.setState({loading: false});
+        })
+    }
+
     render(){
         return (
             <div>
@@ -13,4 +38,4 @@ class Orders extends Component {
     }
 }
 
-export default Orders;
+export default WithErrorHandler(Orders, axios);

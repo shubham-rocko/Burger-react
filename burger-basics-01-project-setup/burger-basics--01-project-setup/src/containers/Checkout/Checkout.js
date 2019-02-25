44605184d29876641762d241eb2adf4f5beeabdc
shubham singh <shubhamit3038@gmail.com>
2019-02-25 12:20:14 +0530

    order submission and data passing between pages

--- burger-basics-01-project-setup/burger-basics--01-project-setup/src/containers/Checkout/Checkout.js
+++ burger-basics-01-project-setup/burger-basics--01-project-setup/src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, withRouter } from 'react-router-dom';
 import ContactData from './ContactData/ContactData';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
@@ -7,21 +7,22 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 class Checkout extends Component {
 
     state = {
-        ingredients: {
-            salad: 1,
-            meat: 1,
-            bacon: 1,
-            cheese: 1
-        }
+        ingredients: null,
+        totalPrice: 0
     };
 
-    componentDidMount() {
+    componentWillMount() {
         var query = new URLSearchParams(this.props.location.search);
         const ingredients = {};
+        let price = 0;
         for (let param of query.entries()) {
-            ingredients[param[0]] = +param[1];
+            if (param[0] === 'price') {
+                price = +param[1];
+            } else {
+                ingredients[param[0]] = +param[1];
+            }
         }
-        this.setState({ ingredients: ingredients });
+        this.setState({ ingredients: ingredients, totalPrice: price });
     }
 
     checkoutCancledHandler = () => {
@@ -40,10 +41,12 @@ class Checkout extends Component {
                     checkoutCancled={this.checkoutCancledHandler}
                     checkoutContinued={this.checkoutContinuedHandler}
                 />
-                <Route path={`${this.props.match.path}/contact-data`} component={ContactData} />
+                <Route path={`${this.props.match.path}/contact-data`}
+                    render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props} />)} />
             </div>
         )
     }
 }
 
 export default Checkout;
+// export default withRouter(Checkout);

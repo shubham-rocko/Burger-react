90638df93987f8ea4fbc585f90a9a4529b35bbe7
shubham singh <shubhamit3038@gmail.com>
2019-02-25 23:19:07 +0530

    handling form submission

--- burger-basics-01-project-setup/burger-basics--01-project-setup/src/containers/Checkout/ContactData/ContactData.js
+++ burger-basics-01-project-setup/burger-basics--01-project-setup/src/containers/Checkout/ContactData/ContactData.js
@@ -66,10 +66,15 @@ class ContactData extends Component {
     orderHandler = (event) => {
         event.preventDefault();
         console.log(event, this.props.ingredients);
+        const formData = {};
+        for(let formElementIdentifier in this.state.orderForm){
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
         this.setState({ loading: true });
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.price
+            price: this.props.price,
+            orderData: formData
         }
         axios.post('/orders.json', order)
             .then((response) => {
@@ -103,7 +108,7 @@ class ContactData extends Component {
             });
         }
 
-        var form = (<form>
+        var form = (<form onSubmit={this.orderHandler}>
             {formElementsArray.map(formElement => (
                 <Input
                     key={formElement.id}
